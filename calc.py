102277f0713f5ade29311ee7654cb4cb4c33e8e0
Nightoo <57502718+Nightoo@users.noreply.github.com>
2020-04-27 12:47:23 +0300

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,81 @@
+import os
+from flask import Flask
+from flask import request
+from flask_ngrok import run_with_ngrok
+
+app = Flask(__name__)
+run_with_ngrok(app)
+
+
+@app.route('/calculator', methods=['POST', 'GET'])
+def calc():
+    if request.method == 'GET':
+        return """<!doctype html>
+                    <html lang="en">
+                      <head>
+                        <meta charset="utf-8">
+                        <title>Калькулятор!</title>
+                      </head>
+                      <body>
+                        <h1>Калькулятор!!!</h1>
+                        <form name="test" method="post" action="/calculator">
+                         <p>
+                          <input type="number" size="15" name="number1" placeholder='Введите число' id="number1">
+                         </p>
+                         <p>
+                          <input type="number" size="15" name="number2" placeholder='Введите число' id="number2">
+                         </p>
+                         <p>
+                          <div class="form-check">
+                           <input class="form-check-input" type="radio" name="operation" id="plus" value="plus" checked>
+                            <label class="form-check-label" for="+">
+                             +
+                            </label>
+                          </div>
+                          <div class="form-check">
+                            <input class="form-check-input" type="radio" name="operation" id="minus" value="minus">
+                             <label class="form-check-label" for="-">
+                              -
+                             </label>
+                          </div>
+                          <div class="form-check">
+                            <input class="form-check-input" type="radio" name="operation" id="multiplication" value="multiplication">
+                             <label class="form-check-label" for="*">
+                              *
+                             </label>
+                          </div>
+                          <div class="form-check">
+                            <input class="form-check-input" type="radio" name="operation" id="division" value="division">
+                             <label class="form-check-label" for="/">
+                              /
+                             </label>
+                          </div>
+                          </p>
+                         <p>
+                          <input type='submit' value='Получить результат'>
+                         </p>
+                        </form>
+                      </body>
+                    </html>"""
+
+    elif request.method == 'POST':
+        print(request.form['number1'])
+        print(request.form['number2'])
+        number1 = int(request.form['number1'])
+        number2 = int(request.form['number2'])
+        print(request.form['operation'])
+        if request.form['operation'] == 'plus':
+            return str(number1 + number2)
+        elif request.form['operation'] == 'minus':
+            return str(number1 - number2)
+        elif request.form['operation'] == 'multiplication':
+            return str(number1 * number2)
+        elif request.form['operation'] == 'division':
+            if number2 == 0:
+                return 'Сладкий, так нельзя'
+            else:
+                return str(number1 / number2)
+
+
+if __name__ == '__main__':
+    app.run()
