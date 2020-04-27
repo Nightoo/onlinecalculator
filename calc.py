344ba8dc4d93d3159627a2c5f565d2c5770176ff
Nightoo <57502718+Nightoo@users.noreply.github.com>
2020-04-27 14:04:39 +0300

    Add files via upload

--- calc.py
+++ calc.py
@@ -73,3 +73,6 @@ def calc():
                 return 'Сладкий, так нельзя'
             else:
                 return str(number1 / number2)
+
+#if __name__ == '__main__':
+#    app.run(port=8080, host='127.0.0.1')
